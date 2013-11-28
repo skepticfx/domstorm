d84cd9ac76218b32425a68c50fd325638a2ca716
skepticfx <ahamednafeez@gmail.com>
2013-11-29 01:48:58 +0530

    New DB

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 // Config File
 
 var config = {};
-config.DB_URL = 'mongodb://nafeez:secret123@ds053788.mongolab.com:53788/dom-storm';
+config.DB_URL = 'mongodb://admin:4fcnQu43EG3n@127.12.118.130:27017/domstorm';
+//mongodb://nafeez:secret123@ds053788.mongolab.com:53788/dom-storm
+//mongodb://admin:4fcnQu43EG3n@127.12.118.130:27017/domstorm
 
 exports.config = config;
